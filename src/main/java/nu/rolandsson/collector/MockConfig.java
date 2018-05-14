a5561b96c82c27b8b5d426c42b3847149403d2c0
Jakob Rolandsson <Kavzor@gmail.com>
2018-05-14 21:43:03 +0200

    re-testing heroku ci

--- src/main/java/nu/rolandsson/collector/MockConfig.java
+++ src/main/java/nu/rolandsson/collector/MockConfig.java
@@ -15,7 +15,7 @@ public class MockConfig {
     return new WeatherService() {
       @Override
       public String getWeather() {
-        return "Clear blue sky today";
+        return "Clear blue sky today, not tomrrow";
       }
     };
   }
